6cc288ac0ae2e440f4e8ca9adb7bc673e9ab45c5
Ekaterina <gaurung505@gmail.com>
2023-10-25 13:48:21 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-
         int ticket = 400;
         int bonus = 20;
         int miles = ticket / bonus;
-
-
         System.out.println("Бесплатные мили:");
         System.out.println(miles);
     }
